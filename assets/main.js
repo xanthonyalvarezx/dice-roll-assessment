927408ad91257da4d332218532ed857051d374ca
AntMan <xanthonyalvarezx@gmail.com>
2020-05-14 17:22:47 -0400

    trying to finish second half/ ordered list

--- assets/main.js
+++ assets/main.js
@@ -1,25 +1,46 @@
-let dieRolls = []//array for roll results
+let dieRolls = [] //array for roll results
 
 
 
-let roll = document.querySelector('#roll')// roll button
-let input = document.querySelector('#input')//input number
+let roll = document.querySelector('#roll') // roll button
+let input = document.querySelector('#input') //input number
 let total = document.querySelector('#total')
+let showRolls = document.querySelector('#show')
 
-
-roll.addEventListener('click', function(){
-    let dieNumber = parseInt(input.value)//the input value
+roll.addEventListener('click', function () {
+    let dieNumber = parseInt(input.value) //the input value
     
     
-    while(dieRolls.length < dieNumber){//while die rolls length is less than te number of die
-        dieRolls.push(Math.floor(Math.random() * 6 + 1 ))  //push a random number to dieRolls array
-
-        let sum = dieRolls.reduce( function(total, amount){
+    while (dieRolls.length < dieNumber) { //while die rolls length is less than te number of die
+        dieRolls.push(Math.floor(Math.random() * 6 + 1)) //push a random number to dieRolls array
+        
+        let sum = dieRolls.reduce(function (total, amount) { //reduce array to 
             return total + amount
-          });
-          total.innerText = sum
-
-      console.log(dieRolls)
+        })
+        total.innerText = sum
+        
+        console.log(dieRolls)
     }
     
+    
+}) // event listner while i < array .length create new html element with inner text of current index.
+showRolls.addEventListener('click', function () {// <= listen for click
+    let i = 0 // let index = 0
+
+    while (i < dieRolls.length ) { // while index is less than the length of the array
+
+        dieRolls.join()// join array to make it a string 
+
+        let newItem = document.createElement('li')// create a newItem variable equal to a  new list item 
+
+       let itemContent = document.createTextNode(dieRolls[i]) // itemContent = text node of array index
+
+       newItem.appendChild(itemContent)  // append new content to new list item
+let list = document.querySelector('#allrolls')
+       document.body.afterStart(newItem, list)
+
+       console.log(list)
+        
+    }
+i++
 })
